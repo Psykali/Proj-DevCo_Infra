46987d60035f8f05984029c874fa7afe863f5162
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-11 12:26:37 +0000

    Updated WebServ-VM.tf

--- TF-Infra/WebServ-VM.tf
+++ TF-Infra/WebServ-VM.tf
@@ -91,16 +91,6 @@ resource "azurerm_public_ip" "web_pip" {
   domain_name_label   = var.web_vm
   tags = local.common_tags
 }
-###################
-## SQL Databases ##
-###################
-resource "azurerm_mysql_database" "web_db" {
-  name                = "webdb"
-  resource_group_name = var.resource_group_name
-  server_name         = azurerm_mysql_server.mysql.name
-  charset             = "UTF8"
-  collation           = "UTF8_GENERAL_CI"
-}
 #######################################################################
 ####################
 ## Bash Scripting ##
