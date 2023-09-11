eb91eb0fbcdace4d621d704b4ac4acb83b1c3e8c
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-11 12:25:17 +0000

    Updated GitLab-VM.tf

--- TF-Infra/GitLab-VM.tf
+++ TF-Infra/GitLab-VM.tf
@@ -91,16 +91,6 @@ resource "azurerm_public_ip" "gitlab_pip" {
   domain_name_label   = var.gitlab_vm
   tags = local.common_tags
 }
-###################
-## SQL Databases ##
-###################
-resource "azurerm_mysql_database" "git_db" {
-  name                = "gitdb"
-  resource_group_name = var.resource_group_name
-  server_name         = azurerm_mysql_server.mysql.name
-  charset             = "UTF8"
-  collation           = "UTF8_GENERAL_CI"
-}
 #######################################################################
 ####################
 ## Bash Scripting ##
